3d986d8dc549d0e45253c574f58bc0472a7f0670
pankajmohanty <51083695+pankajmohanty@users.noreply.github.com>
2024-03-27 11:29:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { createClient } from "@propelauth/javascript";
 
 const authClient = createClient({
     // The base URL where your authentication pages are hosted. You can find this under the Frontend Integration section for your project.
-    authUrl: "https://auth.yourdomain.com",
+    authUrl: "https://auth.fload.tech",
     // If true, periodically refresh the access token in the background. This helps ensure you always have a valid token ready to go. Default true.
     enableBackgroundTokenRefresh: true,
 });
